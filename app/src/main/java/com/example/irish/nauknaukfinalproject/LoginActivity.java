0713f76d72a7e8fd74bed1e537f704f43b161c5d
Daniel Abrahms <dabrahms@zagmail.gonzaga.edu>
2017-12-07 18:14:56 -0800

    Finished professor view (might change colors)

---
+++ app/src/main/java/com/example/irish/nauknaukfinalproject/LoginActivity.java
@@ -0,0 +1,26 @@
+package com.example.irish.nauknaukfinalproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_out_bottom, R.anim.slide_in_top);
+            }
+        });
+    }
+}
